06f1076c308778c8d552e4709099baeed827a0cf
Галеев Ильдар <ildar-galeev92@mail.ru>
2023-07-13 03:42:54 +0700

    changed DATA_JSON_KEEPER_FILE_ID property

--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
 import os
 
 DATA_JSON_FILENAME = os.path.join(os.path.dirname(__file__), 'data', 'words.json')
-DATA_JSON_KEEPER_FILE_ID = ''
+DATA_JSON_KEEPER_FILE_ID = 'UI3U'
 
 MIN_WORD_LENGTH = 3
 STOP_WORDS = ['stop', 'стоп']
