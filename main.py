0b6a12c084a4d2f49c9a884eff325694890716e8
Галеев Ильдар <ildar-galeev92@mail.ru>
2023-07-13 03:44:03 +0700

    implemented a game

--- main.py
+++ main.py
@@ -2,9 +2,50 @@ import utils
 
 
 def main(data_list: list):
+    is_game_running = True
 
-    for word in data_list:
-        print(word)
+    player = utils.get_player()
+    words_generator = utils.get_word_generator(data_list)
+
+    print(f'Привет, {player}\n')
+
+    while is_game_running:
+        word = next(words_generator)
+        subword_count = word.subwords_count
+
+        utils.print_word_prompt(word)
+
+        print('-' * 10)
+
+        while subword_count:
+            user_input = input('Ваше слово: ').lower().strip()
+
+            if not utils.check_user_input(user_input):
+                print('Слишком короткое слово')
+                continue
+
+            if utils.check_stop_word(user_input):
+                is_game_running = False
+                print(f'{utils.FG_YELLOW}Вы угадали {player.usedwords_count} слов!{utils.FG_RESET}')
+                break
+
+            match utils.check_word(player, word, user_input):
+                case None:
+                    print(f'{utils.FG_YELLOW}Уже использовано!{utils.FG_RESET}')
+                case True:
+                    subword_count -= 1
+                    print(f'{utils.FG_GREEN}Правильно!{utils.FG_RESET}')
+                case False:
+                    print(f'{utils.FG_RED}Неправильно!{utils.FG_RESET}')
+
+            if subword_count:
+                print(f'Осталось {utils.FG_MAGENTA}{subword_count}{utils.FG_RESET} слов\n')
+            else:
+                print("Вы молодец! Продолжим!")
+
+        print('-' * 10)
+
+    print(f'Спасибо за игру, {player}')
 
 
 if __name__ == '__main__':
